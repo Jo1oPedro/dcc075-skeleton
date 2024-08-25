9479086898c40f66a83dcfb2cd67f456f5d44663
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-08-25 13:48:07 -0300

    Add global request to the controller abstract class

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,15 @@
 <?php
 
-use Security\Skeleton\Controllers\BankController;
 use Security\Skeleton\Http\Middleware\Authenticate;
 
 if($request->server['REQUEST_METHOD'] == 'GET') {
     switch ($request->server['PATH_INFO'] ?? "/") {
-        case "/":
-            break;
-        case "/bankAccount":
-            try {
-                middleware(Authenticate::class);
-                (new BankController)($request->server["QUERY_STRING"]);
-            } catch (Exception $exception) {
-                return;
-            }
-            break;
+
     };
 }
 
 if($request->server["REQUEST_METHOD"] == "POST") {
     switch ($request->server['PATH_INFO']) {
-        case "/login":
-            middleware();
-            break;
+
     };
 }
